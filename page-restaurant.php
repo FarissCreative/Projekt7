989f829a60f68b67ca9eaddf3c522acbfcb879ba
jonnamh <jonna.mh@hotmail.com>
2022-05-30 18:43:01 +0200

    restaurant underside plus style

---
+++ page-restaurant.php
@@ -0,0 +1,23 @@
+<?php
+  /**
+  *Template for displaying all the pages
+  **/
+  get_header(); ?>
+
+
+  <?php
+    //the post loop
+    if ( have_posts() ){
+      while (have_posts() ){
+        the_post();
+        ?> <section id="restaurant"> <?php
+          the_content();
+        ?> </section> <?php
+      }
+    }
+  ?>
+
+  <?php
+  get_footer();
+
+?>
